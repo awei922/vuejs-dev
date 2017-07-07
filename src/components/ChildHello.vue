9a73c78fb577adc170be303e57279b90770efda0
aweig <awei@aweideMacBook-Air.local>
2017-07-07 22:08:40 +0800

    11

--- src/components/ChildHello.vue
+++ src/components/ChildHello.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="childhello">
+    <!--显示来自父组件的数据-->
     <h2>{{ msg }}</h2>
   </div>
 </template>
@@ -7,14 +8,16 @@
 <script>
 export default {
   name: 'childhello',
+
    data () {
     return {
-      childMsg: '我是子组件，来自子组件'
+      childMsg: '我是子组件，来自子组件'//数据被传送到父组件
     }
   },  
+
   props:['msg'],
   mounted:function(){     
-      this.$emit("childMsg", this.childMsg)       
+      this.$emit("childMsg", this.childMsg) //传递数据到父组件      
       //触发当前实例上的事件。附加参数都会传给监听器回调, vm.$emit( event, […args] );
   }
 }
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="hello">
     <h1>我是父组件</h1>
+    <!--显示组件,childMsg是自义定事件，recieve自定义方法，用于接受子组件的数据-->
     <child-hello :msg="msg" @childMsg="recieve"></child-hello>
+    <!--显示来自子组件的数据-->
     <h2>{{childMsg}}</h2>
   </div>
 </template>
 
 <script>
+//加载子组件
 import ChildHello from './ChildHello.vue'
 
 export default {
-  name: 'hello',
-  components:{
-    ChildHello
+  name: 'hello',//
+
+  components:{//注册组件
+    ChildHello //子组件对于上面的child-hello，这个是vuejs自动转换的
   },
+
   data() {
     return {
-      msg: '我是子组件，来自父组件',
-      childMsg:'',
+      msg: '我是子组件，来自父组件',//消息传递到子组件中
+      childMsg:'',//用于显示来自子组件中的数据
     }
   },
   
