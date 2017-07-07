0c1e20a18cfe8980d5791a0bc8bf8360d214b0f7
刘志伟 <liuzhw@91160.com>
2017-07-07 19:02:45 +0800

    vuedemo

---
+++ src/components/ChildHello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="childhello">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'childhello',
+   data () {
+    return {
+      childMsg: '我是子组件，来自子组件'
+    }
+  },  
+  props:['msg'],
+  mounted:function(){     
+      this.$emit("childMsg", this.childMsg)       
+      //触发当前实例上的事件。附加参数都会传给监听器回调, vm.$emit( event, […args] );
+  }
+}
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>我是父组件</h1>
+    <child-hello :msg="msg" @childMsg="recieve"></child-hello>
+    <h2>{{childMsg}}</h2>
+  </div>
+</template>
+
+<script>
+import ChildHello from './ChildHello.vue'
+
+export default {
+  name: 'hello',
+  components:{
+    ChildHello
+  },
+  data() {
+    return {
+      msg: '我是子组件，来自父组件',
+      childMsg:'',
+    }
+  },
+  
+  methods: {
+    // recieve事件用于接收子组件传过来的参数，参数的个数要和子组件传递过来的一致。
+    recieve: function(res) { 
+      this.childMsg = res;
+    }
+  }
+
+}
+</script>
